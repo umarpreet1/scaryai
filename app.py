47acae98c3e66d006bc5e6f14dfad0eda4434bdb
Umarpreet Singh <umarpreet.singh@mail.utoronto.ca>
2023-02-17 13:23:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import render_template
 from flask import request
 import requests
 import random
+import json
 
 app = Flask(__name__)
 
@@ -15,10 +16,31 @@ def index():  # put application's code here
 
 
 def get_inference(prompt,max_new_tokens):
-    payload = {"prompt":prompt,"max_new_tokens":max_new_tokens}
-    result = requests.post('http://147.182.156.31:5000/generate',params=payload)
-    result = result.json()
-    return result['generated_sequence']
+    #payload = {"prompt":prompt,"max_new_tokens":max_new_tokens}
+    #result = requests.post('http://147.182.156.31:5000/generate',params=payload)
+    #result = result.json()
+    apiKey = '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'
+    model = 'ada:ft-personal-2023-01-31-16-19-18'
+    prompt = prompt
+
+    headers = {
+    'Content-Type': 'application/json',
+    'Authorization': f'Bearer {apiKey}'
+    }
+
+    data = {
+    'model': model,
+    'prompt': prompt,
+    'max_tokens': 30,
+    'temperature': 0.8,
+    'top_p': 0.95,
+    }
+
+    response = requests.post('https://api.openai.com/v1/completions', headers=headers, data=json.dumps(data))
+    json_response = response.json()
+    result = json_response['choices'][0]['text']
+    prompt = prompt + result
+    return prompt
 
 @app.route('/generate',methods=['GET','POST'])
 def generate():
